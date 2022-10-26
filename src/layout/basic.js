2b2ae9fafa92b9cd005bca6912bb797df2d117c7
David Park <davepark97@berkeley.edu>
2022-10-26 14:41:55 -0700

    Define function to create layout of header

--- src/layout/basic.js
+++ src/layout/basic.js
@@ -13,7 +13,36 @@ class BasicLayout {
     parent.appendChild(header).classList.add('header');
     parent.appendChild(main).classList.add('main');
     parent.appendChild(footer).classList.add('footer');
+  }
 
+  get setHeader() {
+    let header = document.querySelector('.header');
+
+    let top = document.createElement('div');
+    let bottom = document.createElement('div');
+
+    header.appendChild(top).classList.add('header-top');
+    header.appendChild(bottom).classList.add('header-bot');
+    
+    let title = document.querySelector('.header-top');
+    title.textContent = "The Bear";
+    
+    let home = document.createElement('button');
+    let menu = document.createElement('button');
+    let about = document.createElement('button'); 
+
+    let options = document.querySelector('.header-bot');
+    options.appendChild(home).classList.add('home-button');
+    options.appendChild(menu).classList.add('menu-button');
+    options.appendChild(about).classList.add('about-button');
+
+    let homeButton = document.querySelector('.home-button');
+    let menuButton = document.querySelector('.menu-button');
+    let aboutButton = document.querySelector('.about-button');
+
+    homeButton.textContent = "Home";
+    menuButton.textContent = "Menu";
+    aboutButton.textContent = "About";
   }
 
 }
