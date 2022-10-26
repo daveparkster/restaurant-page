83cd44cc69e352dcd7edd48bb2944f86ea49afb6
David Park <davepark97@berkeley.edu>
2022-10-25 17:37:33 -0700

    Define class to store all imported classes
    The Restaurant class is defined to store
    all the imported classes. This method will
    help make this program a lot more organized
    and consistent.

---
+++ src/index.js
@@ -0,0 +1,21 @@
+
+import "./styles/index.css"; 
+
+
+import BasicLayout from './layout/basic.js'; 
+
+
+class Restaurant {
+  constructor() {
+
+  }
+
+  get createPage() {
+    const layout = new BasicLayout(); 
+    layout.setLayout;
+  }
+
+}
+
+const restaurant = new Restaurant();
+restaurant.createPage;
