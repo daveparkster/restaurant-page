ce5433e82ecbda0e41e883d9c45ab6975e785b90
David Park <davepark97@berkeley.edu>
2022-10-28 00:08:38 -0700

    Create content of contact and its styling
    When the user clicks on the contact button,
    the getter function: createContact creates
    the layout of contact within the main div
    container. Additionally, the styling of
    the content of the contact is applied.

--- src/layout/main/contact.js
+++ src/layout/main/contact.js
@@ -1,3 +1,5 @@
+import Map from '../../img/map.png';
+
 class Contact {
   constructor() {
 
@@ -7,6 +9,31 @@ class Contact {
     let main = document.querySelector('main');
     let contact = document.createElement('div');
     main.appendChild(contact).classList.add('main-contact');  
+
+    let mainContact = main.querySelector('.main-contact');
+
+    this.createComponents.call(this, mainContact);
+  }
+
+  createComponents(parent) {
+
+    let phoneContainer = document.createElement('div');
+    let locationContainer = document.createElement('div');
+    let mapContainer = document.createElement('div');
+
+    parent.appendChild(phoneContainer).classList.add('phone');
+    parent.appendChild(locationContainer).classList.add('location');
+    parent.appendChild(mapContainer).classList.add('map');
+
+    parent.querySelector('.phone').textContent = `Phone: (123)-456-7890`;
+    parent.querySelector('.location').textContent = `New York City`;
+
+    
+    const mapImage = new Image();
+    mapImage.src = Map;
+    mapImage.arc = "Map";
+
+    parent.querySelector('.map').appendChild(mapImage);
   }
 }
 
