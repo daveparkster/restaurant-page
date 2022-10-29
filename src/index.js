ca34affe4c26f6cc04cbf7c058d7a532eb9b4bd2
David Park <davepark97@berkeley.edu>
2022-10-28 17:42:51 -0700

    Define a property getter for button event listeners
    Create an instance of the Listeners class. Then,
    access the createListeners getter.

--- src/index.js
+++ src/index.js
@@ -1,44 +1,47 @@
 import "./styles/layout.css"; 
 
-
 import BasicLayout from './layout/basic.js'; 
 import Contact from './layout/main/contact.js';
 import Home from './layout/main/home.js';
 import Menu from './layout/main/menu.js';
 
+import Listeners from './layout/listeners.js';
 
 class Restaurant {
   constructor() {
-
+    //placeholder
   }
 
-  get createPage() {
+  createPage() {
     const layout = new BasicLayout(); 
     layout.setLayout;
     layout.setHeader;
     layout.setFooter;
   }
 
-  get addHome() {
+  addHome() {
     const home = new Home();
     home.createHome;
   }
 
-  get addMenu() {
+  addMenu() {
     const menu = new Menu();
     menu.createMenu;
   }
 
-  get addContact() {
+  addContact() {
     const contact = new Contact();
     contact.createContact;
   }
 
+  get listenForEvents() {
+    const listeners = new Listeners(this.addHome, this.addMenu, this.addContact);
+    listeners.createListeners;
+  }
 }
 
 const restaurant = new Restaurant();
-restaurant.createPage;
-//restaurant.addHome;
-//restaurant.addMenu;
-restaurant.addContact;
+restaurant.createPage();
+restaurant.addHome();
+restaurant.listenForEvents;
 
